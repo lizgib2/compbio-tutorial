4eb0157c350c29614df3e39ea065b8c7c737887a
Sue Parkinson <37385131+suzannastep@users.noreply.github.com>
2023-08-24 13:16:31 -0500

    Update babygwas.py

--- babygwas/babygwas.py
+++ babygwas/babygwas.py
@@ -5,34 +5,39 @@ from scipy.stats import linregress
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def simulate_genotypes(N, L=10):
+def simulate_genotypes(number_of_people, number_of_SNPs=10):
     # Function to simulate genotypes
-    ps = npr.beta(4, 4, size=L)
-    G = np.empty((N, L), dtype=int)
+    probabilities = npr.beta(4, 4, size=number_of_SNPs)
+    genotypes = np.empty((number_of_people, number_of_SNPs), dtype=int)
 
-    for i in range(L):
-        gen = npr.binomial(2, ps[i], size=N)
-        G[:, i] = gen
+    for i in range(number_of_SNPs):
+        genotypes[:, i] = npr.binomial(2, probabilities[i], size=number_of_people)
 
-    return G
+    return genotypes
 
-def simulate_disease(G, beta, epsilon):
-    # Simulate disease status for each individual
-    G = (G - G.mean(axis=1, keepdims=True)) / G.std(axis=1, keepdims=True)
-    N, L = G.shape
-    genetic_component = np.dot(G, beta)
-    D = np.random.normal(0, np.sqrt(epsilon), N) + genetic_component
-    D = np.where(D > 0.5, 1, 0)
-    return D
-
-def generate_beta(L, non_zero_effect_size=0.5):
+def true_effect_sizes(L=10, non_zero_effect_size=0.5):
     # Generate a beta vector with zeros everywhere apart from three fixed loci
-    beta = np.zeros(L)
-    fixed_indices = [0, 5, 7]
-    beta[fixed_indices] = [7,4,4]
-    return beta
+    m = np.zeros(L)
+    fixed_indices = [0,5,7]
+    m[fixed_indices] = [7,-4,4]
+    return m
 
-# the code Liz wrote
+def simulate_LDL_levels(genotypes, standard_deviation=12):
+    # Simulate disease status for each individual
+    m = true_effect_sizes()
+    normalized_genotypes = (genotypes - genotypes.mean(axis=1, keepdims=True)) / genotypes.std(axis=1, keepdims=True)
+    number_of_people, number_of_SNPs = normalized_genotypes.shape
+    genetic_component = np.dot(normalized_genotypes, m)
+    random_component = np.random.normal(0, standard_deviation, number_of_people)
+    LDL_levels = random_component + genetic_component + 125
+    return LDL_levels
+
+def create_dataframe(genotypes,LDL_levels):
+  columns = [f"SNP {i+1}" for i in range(10)]
+  df = pd.DataFrame(genotypes,columns=columns)
+  df["LDL Cholesetrol Level"] = ldl_levels
+  return df
+
 def linear_regression(x,y):
     '''
     A simple linear regression of your genotype (x)
