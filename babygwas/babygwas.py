30cd33d56562795d646054372af964982b8a7e16
Sue Parkinson <37385131+suzannastep@users.noreply.github.com>
2023-10-04 13:57:22 -0500

    Update babygwas.py

--- babygwas/babygwas.py
+++ babygwas/babygwas.py
@@ -31,7 +31,13 @@ def simulate_LDL_levels(genotypes, standard_deviation=12):
     genotypes = np.array(genotypes)
     # Simulate disease status for each individual
     m = true_effect_sizes()
-    number_of_people, number_of_SNPs = genotypes.shape
+    if genotypes.shape == 2:
+        #if genotypes from several people, each person is a row, each SNP is a column
+        number_of_people, number_of_SNPs = genotypes.shape
+    else
+        #if genotypes from one people, lenght of the genotype is the # of SNPs
+        number_of_SNPs = len(genotypes) 
+        number_of_people = 1
     genetic_component = np.dot(genotypes, m)
     environmental_component = np.random.normal(0, standard_deviation, number_of_people)
     LDL_levels = environmental_component + genetic_component + 100
